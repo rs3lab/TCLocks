5e958e0623ad1e7ba193769e257b333cb1e0d29a
Vishal Gupta <vishalgupta7972@gmail.com>
2023-04-24 13:01:31 +0000

    Update vbench script

--- src/benchmarks/vbench/run.sh
+++ src/benchmarks/vbench/run.sh
@@ -4,7 +4,9 @@ cd /home/ubuntu/TCLocks/src/benchmarks/vbench
 
 ./mkmounts tmpfs-separate
 
-source /home/ubuntu/TCLocks/src/defaults.sh && export LD_PRELOAD=/usr/local/lib/libjemalloc.so.2 && ./config.py psearchy
+source /home/ubuntu/TCLocks/src/defaults.sh
+export LD_PRELOAD=/usr/local/lib/libjemalloc.so.2 
+./config.py psearchy
 
 sleep 5
 
